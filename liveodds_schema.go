2370dddf782cf983489e7b216b8e5345bdcf9c7c
@damnwidget <oscar.campos@member.fsf.org>
2013-12-15 23:15:39 +0000

    added schema XML parsing and tests

---
+++ liveodds_schema.go
@@ -0,0 +1,116 @@
+// Copyright 2013 Oscar Campos <oscar.campos@member.fsf.org>
+// See LICENSE file for details.
+
+// Package DamnWidget/brinplay is a Go implementation of BetRadar InPlay protocol
+//
+// This package provides a BetRadar InPlay XML Feeds client and high level
+// convenience methods for common operations with BetRadar InPlay
+//
+// This package is just a project to learn Go and is not intended to be used
+// in production environments.
+
+package liveodds
+
+import (
+	"encoding/xml"
+	"time"
+)
+
+// BetRadarLiveOdds is a Struct ready to XML Unmarshal a BetRadarLiveOdds XML
+// message from BetRadar in play live XML Feeds. It can be used just as:
+//
+// v := BetRadarLiveOdds{}
+// xml_msg := `
+//            <BetRadarLiveOdds status="alive" timestamp="1386870302430"
+//                  xmlns="http://www.betradar.com/BetradarLiveOdds">
+//            `
+// err := xml.Unmatshal([]byte(xml_data), &v)
+// if err != nil {
+//     fmt.Printf("error: %v", err)
+// }
+type BetRadarLiveOdds struct {
+	XMLName   xml.Name `xml:"BetradarLiveOdds"`
+	Status    string   `xml:"status,attr"`
+	Timestamp int64    `xml:"timestamp,attr"`
+	XMLNS     string   `xml:"xmlns,attr"`
+	Matches   []Match  `xml:"Match"`
+	OddsType  []OddsType
+}
+
+// BetRadar does not follows the RFC3339 for Epoch Timestamps
+func (t *BetRadarLiveOdds) Epoch() (epoch time.Time) {
+	epoch = time.Unix(t.Timestamp/1000, 0)
+	return epoch
+}
+
+type Match struct {
+	Active    bool   `xml:"active,attr"`
+	BetStatus string `xml:"betstatus,attr"`
+	MatchID   uint32 `xml:"matchid,attr"`
+	MatchTime uint8  `xml:"matchtime,attr"`
+	MsgNR     uint16 `xml:"msgnr,attr,omitempty"`
+	GameScore string `xml:"gamescore,attr,omitempty"`
+	Score     string `xml:"score,attr,omitempty"`
+	Status    string `xml:"status,attr"`
+	Odds      []Odd
+}
+
+type Odd struct {
+	OddsID           uint32 `xml:"id,attr"`
+	Active           bool   `xml:"active,attr"`
+	Changed          string `xml:"changed,attr"`
+	Combination      uint8  `xml:"combination,attr"`
+	FreeText         string `xml:"freetext,attr"`
+	SpecialOddsValue string `xml:"specialoddsvalue,attr"`
+	SubType          uint16 `xml:"subtype,attr"`
+	Type             string `xml:"type,attr"`
+	TypeID           uint16 `xml:"typeid,attr"`
+	OddsField        []OddsField
+}
+
+type OddsField struct {
+	Value  float32 `xml:",chardata"`
+	Active bool    `xml:"active,attr,omitempty"`
+	Type   string  `xml:"type,attr"`
+}
+
+type OddsType struct {
+	Type      string `xml:"type,attr"`
+	FreeText  string `xml:"freetext,attr,omitempty"`
+	TypeID    uint16 `xml:"typeid,attr"`
+	OddsField []TranslationOddsField
+}
+
+type TranslationOddsField struct {
+	Type string `xml:"type,attr"`
+	Name []Name
+}
+
+type Name struct {
+	Value string `xml:",chardata"`
+	Lang  string `xml:"lang,attr"`
+}
+
+type Card struct {
+	CardID uint32 `xml:"id,attr"`
+	Player string `xml:"player,attr"`
+	Team   string `xml:"team,attr"`
+	Time   uint8  `xml:"time,attr"`
+	Type   string `xml:"type,attr"`
+}
+
+type Score struct {
+	ScoreID     uint32 `xml:"id,attr"`
+	Away        uint8  `xml:"away,attr"`
+	Home        uint8  `xml:"home,attr"`
+	ScoringTeam string `xml:"scoringteam,attr"`
+	Time        uint8  `xml:"time,attr"`
+	Type        string `xml:"type,attr"`
+}
+
+type BookMakerStatus struct {
+	XMLName     xml.Name `xml:"BookMakerStatus"`
+	Timestamp   int64    `xml:"timestamp,attr"`
+	Type        string   `xml:"type,attr"`
+	BookmakerID uint16   `xml:"bookmakerid,attr"`
+}
---
+++ liveodds_schema_test.go
@@ -0,0 +1,198 @@
+package liveodds
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"testing"
+	"time"
+)
+
+// We need this to can use whatever result we need
+type CurrentResult interface{}
+type ExpectedResult interface{}
+
+type xmlTest struct {
+	n        CurrentResult
+	expected ExpectedResult
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func LoadXMLFixture(fixture string) (f BetRadarLiveOdds) {
+	msg, err := ioutil.ReadFile(fixture)
+	check(err)
+
+	feed := BetRadarLiveOdds{}
+	unmarshal_err := xml.Unmarshal(msg, &feed)
+	if unmarshal_err != nil {
+		panic(unmarshal_err)
+	}
+	return feed
+}
+
+func TestSimpleAlive(t *testing.T) {
+
+	feed := LoadXMLFixture("fixtures/alive.xml")
+
+	ti := time.Date(2013, time.December, 15, 17, 56, 30, 0, time.Local)
+	var xmlTests = []xmlTest{
+		{feed.Status, "alive"},
+		{len(feed.Matches), 1},
+		{ti, feed.Epoch()},
+		{feed.XMLNS, "http://www.betradar.com/BetradarLiveOdds"},
+	}
+
+	for _, tt := range xmlTests {
+		if tt.n != tt.expected {
+			t.Errorf("TestSimpleAlive: expected %v, got %v", tt.expected, tt.n)
+		}
+	}
+
+}
+
+func TestSimpleChange(t *testing.T) {
+
+	feed := LoadXMLFixture("fixtures/change.xml")
+	var xmlTests = []xmlTest{
+		{feed.Status, "change"},
+		{len(feed.Matches), 1},
+		{feed.Matches[0].Active, true},
+		{feed.Matches[0].BetStatus, "stopped"},
+		{feed.Matches[0].MatchID, uint32(867278)},
+		{feed.Matches[0].MsgNR, uint16(2)},
+		{feed.Matches[0].Score, "-:-"},
+		{feed.Matches[0].Status, "not_started"},
+		{len(feed.Matches[0].Odds), 5},
+
+		// Odds[0]
+		{feed.Matches[0].Odds[0].Active, true},
+		{feed.Matches[0].Odds[0].Changed, "false"},
+		{feed.Matches[0].Odds[0].Combination, uint8(0)},
+		{feed.Matches[0].Odds[0].FreeText, "Next goal"},
+		{feed.Matches[0].Odds[0].OddsID, uint32(78557)},
+		{feed.Matches[0].Odds[0].SpecialOddsValue, "0:0"},
+		{feed.Matches[0].Odds[0].SubType, uint16(13)},
+		{feed.Matches[0].Odds[0].Type, "ft3w"},
+		{feed.Matches[0].Odds[0].TypeID, uint16(6)},
+
+		// OddsField[0]
+		{feed.Matches[0].Odds[0].OddsField[0].Active, true},
+		{feed.Matches[0].Odds[0].OddsField[0].Type, "1"},
+		{feed.Matches[0].Odds[0].OddsField[0].Value, float32(1.4)},
+		// OddsField[1]
+		{feed.Matches[0].Odds[0].OddsField[1].Active, true},
+		{feed.Matches[0].Odds[0].OddsField[1].Type, "x"},
+		{feed.Matches[0].Odds[0].OddsField[1].Value, float32(7.0)},
+		// OddsField[2]
+		{feed.Matches[0].Odds[0].OddsField[2].Active, true},
+		{feed.Matches[0].Odds[0].OddsField[2].Type, "2"},
+		{feed.Matches[0].Odds[0].OddsField[2].Value, float32(4.05)},
+
+		// Odds[1]
+		{feed.Matches[0].Odds[1].Active, true},
+		{feed.Matches[0].Odds[1].Changed, "false"},
+		{feed.Matches[0].Odds[1].Combination, uint8(0)},
+		{feed.Matches[0].Odds[1].FreeText, ""},
+		{feed.Matches[0].Odds[1].OddsID, uint32(78558)},
+		{feed.Matches[0].Odds[1].SpecialOddsValue, "2.5"},
+		{feed.Matches[0].Odds[1].SubType, uint16(0)},
+		{feed.Matches[0].Odds[1].Type, "to"},
+		{feed.Matches[0].Odds[1].TypeID, uint16(5)},
+
+		// OddsField[0]
+		{feed.Matches[0].Odds[1].OddsField[0].Active, true},
+		{feed.Matches[0].Odds[1].OddsField[0].Type, "o"},
+		{feed.Matches[0].Odds[1].OddsField[0].Value, float32(2.4)},
+		// OddsField[1]
+		{feed.Matches[0].Odds[1].OddsField[1].Active, true},
+		{feed.Matches[0].Odds[1].OddsField[1].Type, "u"},
+		{feed.Matches[0].Odds[1].OddsField[1].Value, float32(1.45)},
+
+		// Odds[2]
+		{feed.Matches[0].Odds[2].Active, true},
+		{feed.Matches[0].Odds[2].Changed, "false"},
+		{feed.Matches[0].Odds[2].Combination, uint8(0)},
+		{feed.Matches[0].Odds[2].FreeText, "Halftime - Who wins the rest?"},
+		{feed.Matches[0].Odds[2].OddsID, uint32(79538)},
+		{feed.Matches[0].Odds[2].SpecialOddsValue, "0:0"},
+		{feed.Matches[0].Odds[2].SubType, uint16(20)},
+		{feed.Matches[0].Odds[2].Type, "ft3w"},
+		{feed.Matches[0].Odds[2].TypeID, uint16(6)},
+
+		// OddsField[0]
+		{feed.Matches[0].Odds[2].OddsField[0].Active, true},
+		{feed.Matches[0].Odds[2].OddsField[0].Type, "1"},
+		{feed.Matches[0].Odds[2].OddsField[0].Value, float32(2.0)},
+		// OddsField[1]
+		{feed.Matches[0].Odds[2].OddsField[1].Active, true},
+		{feed.Matches[0].Odds[2].OddsField[1].Type, "x"},
+		{feed.Matches[0].Odds[2].OddsField[1].Value, float32(2.15)},
+		// OddsField[2]
+		{feed.Matches[0].Odds[2].OddsField[2].Active, true},
+		{feed.Matches[0].Odds[2].OddsField[2].Type, "2"},
+		{feed.Matches[0].Odds[2].OddsField[2].Value, float32(6.75)},
+
+		// Odds[3]
+		{feed.Matches[0].Odds[3].Active, true},
+		{feed.Matches[0].Odds[3].Changed, "false"},
+		{feed.Matches[0].Odds[3].Combination, uint8(0)},
+		{feed.Matches[0].Odds[3].FreeText, "Who wins the rest of the match?"},
+		{feed.Matches[0].Odds[3].OddsID, uint32(78560)},
+		{feed.Matches[0].Odds[3].SpecialOddsValue, "0:0"},
+		{feed.Matches[0].Odds[3].SubType, uint16(4)},
+		{feed.Matches[0].Odds[3].Type, "ft3w"},
+		{feed.Matches[0].Odds[3].TypeID, uint16(6)},
+
+		// OddsField[0]
+		{feed.Matches[0].Odds[3].OddsField[0].Active, true},
+		{feed.Matches[0].Odds[3].OddsField[0].Type, "1"},
+		{feed.Matches[0].Odds[3].OddsField[0].Value, float32(1.45)},
+		// OddsField[1]
+		{feed.Matches[0].Odds[3].OddsField[1].Active, true},
+		{feed.Matches[0].Odds[3].OddsField[1].Type, "x"},
+		{feed.Matches[0].Odds[3].OddsField[1].Value, float32(3.65)},
+		// OddsField[2]
+		{feed.Matches[0].Odds[3].OddsField[2].Active, true},
+		{feed.Matches[0].Odds[3].OddsField[2].Type, "2"},
+		{feed.Matches[0].Odds[3].OddsField[2].Value, float32(7.25)},
+
+		// Odds[4]
+		{feed.Matches[0].Odds[4].Active, true},
+		{feed.Matches[0].Odds[4].Changed, "true"},
+		{feed.Matches[0].Odds[4].Combination, uint8(0)},
+		{feed.Matches[0].Odds[4].FreeText, "Which team has kick off?"},
+		{feed.Matches[0].Odds[4].OddsID, uint32(78559)},
+		{feed.Matches[0].Odds[4].SpecialOddsValue, "-1"},
+		{feed.Matches[0].Odds[4].SubType, uint16(2)},
+		{feed.Matches[0].Odds[4].Type, "ft2w"},
+		{feed.Matches[0].Odds[4].TypeID, uint16(7)},
+
+		// OddsField[0]
+		{feed.Matches[0].Odds[4].OddsField[0].Active, true},
+		{feed.Matches[0].Odds[4].OddsField[0].Type, "1"},
+		{feed.Matches[0].Odds[4].OddsField[0].Value, float32(1.8)},
+		// OddsField[1]
+		{feed.Matches[0].Odds[4].OddsField[1].Active, true},
+		{feed.Matches[0].Odds[4].OddsField[1].Type, "2"},
+		{feed.Matches[0].Odds[4].OddsField[1].Value, float32(1.8)},
+	}
+
+	for _, tt := range xmlTests {
+		if tt.n != tt.expected {
+			t.Errorf("TestSimpleChange: expected %v, got %v", tt.expected, tt.n)
+		}
+	}
+}
+
+func TestSimpleTranslation(t *testing.T) {
+	feed := LoadXMLFixture("fixtures/translation.xml")
+	var xmlTests = []xmlTest{
+		{feed.Status, "translation"},
+		{len(feed.OddsType), 2},
+	}
+}
